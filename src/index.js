cf9a85cfb53581332d25366b0a84226a6e69b612
Juel Raju <JuelMacIBM@Juels-MacBook-Pro.local>
2017-08-22 11:08:29 -0500

    Juel0822_1 update nav

--- src/index.js
+++ src/index.js
@@ -3,18 +3,27 @@ import ReactDOM from 'react-dom';
 import SvgIcon from 'react-icons-kit';
 import { ic_aspect_ratio } from 'react-icons-kit/md/ic_aspect_ratio';
 import { ic_business } from 'react-icons-kit/md/ic_business';
-import { css } from 'aphrodite'
-import { styles } from './components/styles.css'
+import { css } from 'aphrodite';
+import { styles } from './components/styles.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Provider,observer } from 'mobx-react'
-import { initStore } from '../store'
+import { Provider,observer } from 'mobx-react';
+import { initStore } from '../store';
+
+//Icons
+import HomeI from 'react-icons/lib/fa/home';
+import CustI from 'react-icons/lib/fa/user';
+import DOI from 'react-icons/lib/fa/phone-square';
+import ROI from 'react-icons/lib/fa/phone';
+import RGI from 'react-icons/lib/fa/user-secret';
 
 
+//Import Components
 import Customer from './components/Customer/customer';
 import Donor from './components/DO/donor';
 import Recipent from './components/RO/recipient';
 import Regulator from './components/Regulator/regulator';
 import Home from './components/Home/home';
+
 
 
 
@@ -90,11 +99,11 @@ export default class RR4 extends React.Component {
                                 <div className={css(styles.navBar1)}>
 
                                     <ul className={css(styles.menuBar)}>
-                                        <li className={css(styles.menuItem)}><Link className={css(styles.menuIcon)} to="/">Home</Link></li>
-                                        <li className={css(styles.menuItem)} onClick={this.customerF}><Link className={css(styles.menuIcon)} to="/Customer">Customer</Link></li>
-                                        <li className={css(styles.menuItem)} onClick={this.donorMNPGet} ><Link className={css(styles.menuIcon)} to="/DO">Donor CSP</Link></li>
-                                        <li className={css(styles.menuItem)} onClick={this.recepMNPGet}><Link className={css(styles.menuIcon)} to="/RO">Recipient CSP</Link></li>
-                                        <li className={css(styles.menuItem)}><Link className={css(styles.menuIcon)} to="/Regulator">Regulator</Link></li>
+                                        <li className={css(styles.menuItem)}><Link className={css(styles.menuIcon)} to="/"><HomeI/>  Home</Link></li>
+                                        <li className={css(styles.menuItem)} onClick={this.customerF}><Link className={css(styles.menuIcon)} to="/Customer"><CustI/>  Customer</Link></li>
+                                        <li className={css(styles.menuItem)} onClick={this.donorMNPGet} ><Link className={css(styles.menuIcon)} to="/DO"><DOI/>  Donor CSP</Link></li>
+                                        <li className={css(styles.menuItem)} onClick={this.recepMNPGet}><Link className={css(styles.menuIcon)} to="/RO"><ROI/>  Recipient CSP</Link></li>
+                                        <li className={css(styles.menuItem)}><Link className={css(styles.menuIcon)} to="/Regulator"><RGI/>  Regulator</Link></li>
 
                                     </ul>
                                 </div>
